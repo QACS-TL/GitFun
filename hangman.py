9e9c5be983b8768840e1c1bcd1dabeba2191ad0c
PBehague <p_behague@hotmail.com>
2024-12-13 15:31:54 +0000

    spelling fixed

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ import random
 # List of words for the game
 words = ["hangman", "python", "game", "programming", "openai"]
 # comment
-print("sytuff")
+print("stuff")
 # Hangman graphics
 hangman_graphics = [
     '''
